74f78f16cca693a3da66738ec19dccd75f413540
gazlln <gazlong@me.com>
2023-01-19 22:27:30 +0000

    add workshop

--- src/workshop/index.js
+++ src/workshop/index.js
@@ -5,6 +5,11 @@ import "../shared/bubble.js";
 import workShopExersice from "./workshopExersice.js";
 
 //Set up
+const modes = ["most", "secondMost", "thirdMost"];
+
+var currentPageIndex = 0;
+
+//Get ref to dynamic content
 const guidanceOneElement = document.querySelector("#guidanceOneElement");
 const guidanceTwoElement = document.querySelector("#guidanceTwoElement");
 const guidanceThreeElement = document.querySelector("#guidanceThreeElement");
@@ -12,22 +17,107 @@ const guidanceFourElement = document.querySelector("#guidanceFourElement");
 const variableGuidanceElement = document.querySelector(
   "#variableGuidanceElement"
 );
-const pageNumberElement = document.querySelector("#pageNumberElement");
+const backButton = document.querySelector("#back-button");
+const nextButton = document.querySelector("#next-button");
+const tagLineElement = document.querySelector("#tagLineElement");
+
+const textAreaTheValue = document.querySelector("#textArea-theValue");
+const textAreaWhyImportant = document.querySelector("#textArea-whyImportant");
+const textAreaIdeaOne = document.querySelector("#textArea-ideaOne");
+const textAreaIdeaTwo = document.querySelector("#textArea-ideaTwo");
+const textAreaIdeaThree = document.querySelector("#textArea-ideaThree");
+
+//Listen for events
+backButton.addEventListener("click", (e) => {
+  loadPage(currentPageIndex - 1);
+});
+
+nextButton.addEventListener("click", (e) => {
+  loadPage(currentPageIndex + 1);
+});
+
+//List for textarea changes
+textAreaTheValue.addEventListener("input", function () {
+  workShopExersice[modes[currentPageIndex]]["theValue"]["answer"] = this.value;
+});
+
+textAreaWhyImportant.addEventListener("input", function () {
+  workShopExersice[modes[currentPageIndex]]["whyImportant"]["answer"] =
+    this.value;
+});
+
+textAreaIdeaOne.addEventListener("input", function () {
+  workShopExersice[modes[currentPageIndex]]["threeIdeas"]["answer1"] =
+    this.value;
+});
+textAreaIdeaTwo.addEventListener("input", function () {
+  workShopExersice[modes[currentPageIndex]]["threeIdeas"]["answer2"] =
+    this.value;
+});
+textAreaIdeaThree.addEventListener("input", function () {
+  workShopExersice[modes[currentPageIndex]]["threeIdeas"]["answer3"] =
+    this.value;
+});
 
 //On page load set the default content
 function initialisePage() {
-  guidanceOneElement.innerHTML = workShopExersice.guidanceOne;
-  guidanceTwoElement.innerHTML = workShopExersice.guidanceTwo;
-  guidanceThreeElement.innerHTML = workShopExersice.guidanceThree;
-  guidanceFourElement.innerHTML = workShopExersice.guidanceFour;
+  guidanceOneElement.innerHTML = workShopExersice.instructions;
+  guidanceTwoElement.innerHTML = workShopExersice.most.instructions;
+  guidanceThreeElement.innerHTML = workShopExersice.most.theValue.question;
+  guidanceFourElement.innerHTML = workShopExersice.most.whyImportant.question;
 }
 
 //User has selected another page so change it!
 function loadPage(pageIndex) {
+  pageIndex =
+    pageIndex < 0
+      ? 0
+      : pageIndex >= modes.length
+      ? modes.length - 1
+      : pageIndex;
+
+  currentPageIndex = pageIndex;
+
+  //Set the required variable question
+
+  //Variable guidance
   variableGuidanceElement.innerHTML =
-    workShopExersice.variableGuidance[pageIndex - 1].text;
-  pageNumberElement.innerHTML = pageIndex;
+    workShopExersice[modes[pageIndex]]["instructions"];
+
+  //Tag line at bottom of page
+  tagLineElement.innerHTML = workShopExersice[modes[pageIndex]]["tagLine"];
+
+  //The Value
+  textAreaTheValue.value =
+    workShopExersice[modes[pageIndex]]["theValue"]["answer"];
+
+  //Why important
+  textAreaWhyImportant.value =
+    workShopExersice[modes[pageIndex]]["whyImportant"]["answer"];
+
+  //Three ideas
+  textAreaIdeaOne.value =
+    workShopExersice[modes[pageIndex]]["threeIdeas"]["answer1"];
+
+  textAreaIdeaTwo.value =
+    workShopExersice[modes[pageIndex]]["threeIdeas"]["answer2"];
+
+  textAreaIdeaThree.value =
+    workShopExersice[modes[pageIndex]]["threeIdeas"]["answer3"];
+
+  //Sort buttons out.
+  if (pageIndex > 0) {
+    backButton.classList.remove("opacity-0");
+  } else {
+    backButton.classList.add("opacity-0");
+  }
+
+  if (pageIndex < modes.length - 1) {
+    nextButton.classList.remove("opacity-0");
+  } else {
+    nextButton.classList.add("opacity-0");
+  }
 }
 
 initialisePage();
-loadPage(1);
+loadPage(currentPageIndex);
--- src/workshop/workshopExersice.js
+++ src/workshop/workshopExersice.js
@@ -1,25 +1,71 @@
 const workShopExersice = {
-  guidanceOne: `Team Exercises – thinking about your future brand comms campaign
+  instructions: `Team Exercises – thinking about your future brand comms campaign
     please think about which 3 values are most important to focus on communicating.
     Within this please think about the specific element(s) of the value that is most important (the three words on each value card)`,
-  guidanceTwo: `The Value (and the focus within the value i.e. which element):`,
-  guidanceThree: `Why this value is important to your customers or future customers:`,
-  guidanceFour: `Three ideas on the messages that relate to this value:`,
-  variableGuidance: [
-    {
-      code: "most",
-      text: `Most Important Value To Communicate Through Brand Comms…`,
+
+  most: {
+    instructions: "Most Important Value To Communicate Through Brand Comms…",
+    tagLine: "your most important value",
+    theValue: {
+      question:
+        "The Value (and the focus within the value i.e. which element):",
+      answer: "",
     },
-    {
-      code: "secondMost",
-      text: `2nd Most Important Value To Communicate Through Brand Comms…`,
+    whyImportant: {
+      question:
+        "Why this value is important to your customers or future customers:",
+      answer: "",
     },
-    {
-      code: "thirdMost",
-      text: `3rd Most Important Value To Communicate Through Brand Comms…
-        `,
+    threeIdeas: {
+      question: "Three ideas on the messages that relate to this value:",
+      answer1: "",
+      answer2: "",
+      answer3: "",
     },
-  ],
+  },
+
+  secondMost: {
+    tagLine: "your 2nd most important value",
+    instructions:
+      "2nd Most Important Value To Communicate Through Brand Comms…",
+    theValue: {
+      question:
+        "The Value (and the focus within the value i.e. which element):",
+      answer: "",
+    },
+    whyImportant: {
+      question:
+        "Why this value is important to your customers or future customers:",
+      answer: "",
+    },
+    threeIdeas: {
+      question: "Three ideas on the messages that relate to this value:",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+    },
+  },
+  thirdMost: {
+    tagLine: "your 3rd most important value",
+    instructions:
+      "3rd Most Important Value To Communicate Through Brand Comms…",
+    theValue: {
+      question:
+        "The Value (and the focus within the value i.e. which element):",
+      answer: "",
+    },
+    whyImportant: {
+      question:
+        "Why this value is important to your customers or future customers:",
+      answer: "",
+    },
+    threeIdeas: {
+      question: "Three ideas on the messages that relate to this value:",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+    },
+  },
 };
 
 export default workShopExersice;
